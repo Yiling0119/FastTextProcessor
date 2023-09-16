e0a62db020366a986dbc1950844b2d7796a423e7
Yiling0119 <112362327+Yiling0119@users.noreply.github.com>
2023-09-16 17:21:03 +0800

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="FastTextProcessor",
+    version="2.0.0",
+    description="A tool for processing text data in various formats.",
+    author="Loh Yi Ling",
+    author_email="lohyilingg0119@gmail.com",
+    packages=find_packages(),
+    install_requires=[
+        "tqdm",
+    ],
+    entry_points={
+        "console_scripts": [
+            "fasttextprocessor = FastTextProcessor:main",
+        ],
+    },
+)
